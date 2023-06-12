13a22ddc47df07a76465e50af1015fbaa07ef6ab
Dejan Stojanovic <zasluzujene@gmail.com>
2023-06-12 18:10:11 +0200

    Treci komit

--- PASSGEN/settings.py
+++ PASSGEN/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&q&t%vqo(#48449gm19uw-i958u=avbqg2x_(f!xez_*6*&%=%"
+SECRET_KEY = "PRVIdjango-insecure-&q&t%vqo(#48449gm19uw-i958u=avbqg2x_(f!xez_*6*&%=%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
